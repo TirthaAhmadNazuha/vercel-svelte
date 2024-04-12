84e3b5944a811db1ed9a47cc11104614a0e94006
TirthaAhmadNazuha <tirthaahmadnazuha.udah@gmail.com>
2024-04-12 17:36:16 +0700

    update

--- api/hello/route.js
+++ api/hello/route.js
@@ -1,10 +1,3 @@
-import vercel from '@vercel/node'
-
-/**
- * 
- * @param {vercel.VercelRequest} request 
- * @param {vercel.VercelResponse} response 
- */
 export async function GET(request, response) {
     console.dir(request)
     return new Response('hello world')
